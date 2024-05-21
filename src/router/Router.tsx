190dc0a17fb1327921d2b73124f22adc00f7ff51
Michaël Bourjac <michael.bourjac@gmail.com>
2024-05-21 19:43:31 +0200

    feat: pass query client from router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,11 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 import { useAuthStore } from '../services/auth/auth.store';
 import { router } from './router.instance';
 
 export const Router = () => {
+  const queryClient = useQueryClient();
   const auth = useAuthStore((state) => state.auth);
 
-  return <RouterProvider router={router} context={{ auth }} />;
+  return <RouterProvider router={router} context={{ auth, queryClient }} />;
 };
--- src/router/router.context.ts
+++ src/router/router.context.ts
@@ -1,16 +1,15 @@
 import type { QueryClient } from '@tanstack/react-query';
-import { queryClient } from '../lib/react-query.client';
 import type { Auth } from '../services/auth/auth.types';
 import { userService } from '../services/user/user.service';
 
 export type RouterContext = {
-  auth: Auth;
   queryClient: QueryClient;
+  auth: Auth;
   userService: typeof userService;
 };
 
 export const routerContext: RouterContext = {
+  queryClient: undefined!,
   auth: undefined!,
-  queryClient,
   userService,
 };
